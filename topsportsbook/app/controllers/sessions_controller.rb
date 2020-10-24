f6cf0d85df30e229a98c202aab8950fdd779bf9b
glennanj1 <61764500+glennanj1@users.noreply.github.com>
2020-10-24 18:34:14 -0400

    added password digest migration to users table, error fixed

--- topsportsbook/app/controllers/sessions_controller.rb
+++ topsportsbook/app/controllers/sessions_controller.rb
@@ -4,4 +4,15 @@ class SessionsController < ApplicationController
         erb :"sessions/login"
     end
 
+    post '/login' do
+        
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/bets"
+        else
+            redirect "/login"
+        end
+    end
+
 end
--- topsportsbook/db/schema.rb
+++ topsportsbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_053859) do
+ActiveRecord::Schema.define(version: 2020_10_24_223237) do
 
   create_table "bets", force: :cascade do |t|
     t.integer "amount"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_24_053859) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
