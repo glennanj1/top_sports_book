fea202fafa8ba127ad7d62d5b9acb008573af107
glennanj1 <61764500+glennanj1@users.noreply.github.com>
2020-10-23 21:47:30 -0400

    Created database tables then migrated them.

--- topsportsbook/db/migrate/20201024013402_create_users.rb
+++ topsportsbook/db/migrate/20201024013402_create_users.rb
@@ -1,5 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    
+    create_table :users do |t|
+      t.string :username
+      t.text :password_digest
+
+      t.timestamps null: false
+    end
   end
 end
--- topsportsbook/db/migrate/20201024013411_create_bets.rb
+++ topsportsbook/db/migrate/20201024013411_create_bets.rb
@@ -1,5 +1,9 @@
 class CreateBets < ActiveRecord::Migration[5.2]
   def change
-
+    create_table :bets do |t|
+      t.integer :amount
+      t.string :team
+      t.string :date
+    end
   end
 end
