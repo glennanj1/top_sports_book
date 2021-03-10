0c66f8f9ed0a93353e63ac273d5650b479c64080
github username <github email address>
2021-03-10 17:56:48 -0500

    added pg to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_11_155851) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bets", force: :cascade do |t|
     t.integer "amount"
     t.string "team"
